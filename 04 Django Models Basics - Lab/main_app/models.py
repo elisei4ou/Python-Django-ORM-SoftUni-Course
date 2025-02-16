e0045d89d1e2a080c10b8244bf7f43b8bbae2ac6
elisei4ou <elisei.29@abv.bg>
2025-02-16 11:51:54 +0200

    Some Changes

--- 04 Django Models Basics - Lab/main_app/models.py
+++ 04 Django Models Basics - Lab/main_app/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-
+from datetime import date
 
 class Employee(models.Model):
     name = models.CharField(max_length=30)
@@ -22,4 +22,15 @@ class Department(models.Model):
     name = models.CharField(max_length=50, unique=True)
     employees_count = models.PositiveIntegerField(default=1, verbose_name='Employees Count')
     location = models.CharField(max_length=20, null=True, blank=True, choices=Cities.choices)
+    last_edited_on = models.DateTimeField(auto_now=True, editable=False)
+
+
+class Project(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+    description = models.TextField(null=True, blank=True)
+    budget = models.DecimalField(max_digits=10, decimal_places=2, null=True, blank=True)
+    duration_in_days = models.PositiveIntegerField(null=True, blank=True, verbose_name='Duration in Days')
+    estimated_hours = models.FloatField(null=True, blank=True, verbose_name='Estimated Hours')
+    start_date = models.DateField(null=True, blank=True, verbose_name='Start Date', default=date.today())
+    created_on = models.DateTimeField(auto_now_add=True, editable=False)
     last_edited_on = models.DateTimeField(auto_now=True, editable=False)
